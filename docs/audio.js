ad1d607bd617851f20bf7fca1fd710723806a965
Kevin Rose <kevin+gh@maypark.com>
2020-03-22 16:13:32 -0500

    pure TFJS impl of interpolation

--- docs/audio.js
+++ docs/audio.js
@@ -6,102 +6,102 @@ async function loadModel() {
   return await tf.loadLayersModel("/models/model.json");
 }
 
-function interpolate(y, inFs, outFs) {
-  if (y.length < 2) {
-    throw "y must have length >= 2";
-  }
-  yNew = new Array(Math.ceil(((y.length - 1) * inFs) / outFs) + 1);
-  x = 0;
-  x0 = 0;
-  x1 = inFs;
-  oldIndex = 0;
-  for (i = 0; i < yNew.length; i++) {
-    while (x1 < i * outFs) {
-      x0 = x1;
-      x1 += inFs;
-      oldIndex++;
-    }
-    alpha = (x - x0) / inFs;
-    yNew[i] = (1 - alpha) * y[oldIndex] + alpha * y[oldIndex + 1];
-    x += outFs;
-  }
-  yNew[yNew.length - 1] = y[y.length - 1];
-  return yNew;
-}
-
-class Spectrogram extends tf.layers.Layer {
-  constructor(config) {
-    super(config || {});
-    this.frameLength = config["frameLength"];
-    this.frameStep = config["frameStep"];
-  }
-
-  computeOutputShape(_inputShape) {
-    return [null, null, this.frameLength / 2 + 1];
-  }
-
-  call(xList) {
-    if (Array.isArray(xList)) {
-      // TFJS wraps inputs into lists...
-      var x = xList[0];
-    } else {
-      var x = xList;
-    }
-    if (x.shape[0] !== 1) {
-      throw "TFJS's stft function only supports 1 dimension.";
-    }
-    return tf
-      .sqrt(
-        tf.abs(tf.signal.stft(x.flatten(), this.frameLength, this.frameStep))
-      )
-      .expandDims();
-  }
-
-  static get className() {
-    return "Spectrogram";
-  }
-}
-tf.serialization.registerClass(Spectrogram);
-
 if (navigator.mediaDevices) {
-  console.log("getUserMedia supported.");
-  loadModel()
-    .then(function(model) {
-      navigator.mediaDevices
-        .getUserMedia({ audio: true })
-        .then(function(stream) {
-          const audioCtx = new AudioContext();
-          const source = audioCtx.createMediaStreamSource(stream);
+  navigator.mediaDevices
+    .getUserMedia({ audio: true })
+    .then(function(stream) {
+      const audioCtx = new AudioContext();
+      const source = audioCtx.createMediaStreamSource(stream);
 
-          var audioBuffer = new Array(audioCtx.sampleRate * 20).fill(0);
+      class Spectrogram extends tf.layers.Layer {
+        constructor(config) {
+          super(config || {});
+          this.frameLength = config["frameLength"];
+          this.frameStep = config["frameStep"];
+        }
 
-          scriptNode = audioCtx.createScriptProcessor(16384, 1, 1);
-          scriptNode.onaudioprocess = function(event) {
-            resampled = interpolate(
-              event.inputBuffer.getChannelData(0),
-              1 / audioCtx.sampleRate,
-              1 / modelSampleRate
-            );
-            audioBuffer = audioBuffer.slice(resampled.length).concat(resampled);
+        computeOutputShape(_inputShape) {
+          return [null, null, this.frameLength / 2 + 1];
+        }
 
-            // 1. Resample to desired sample rate (30,000 Hz)
-            // 2. Call model on audioBuffer
-            functionOutput.innerHTML = model.predict(
-              tf.tensor(audioBuffer, [1, audioBuffer.length])
-            );
-            console.log(audioBuffer[0]);
-          };
+        call(yList) {
+          if (Array.isArray(yList)) {
+            // TFJS wraps inputs into lists...
+            var y = yList[0];
+          } else {
+            var y = yList;
+          }
+          if (y.shape[0] !== 1) {
+            throw "TFJS's stft function only supports 1 dimension.";
+          }
+          y = y.flatten();
 
-          source.connect(scriptNode);
-          scriptNode.connect(audioCtx.destination);
-        })
-        .catch(function(err) {
-          console.log("Error with getUserMedia: ", err);
-        });
+          // Resample.
+          // This is not part of the python implementation of Spectrogram.
+          // Compute the 1 / new sample rate, as if the original sampling
+          // rate were exactly 1. This makes some stuff easier.
+          const relativeFs = modelSampleRate / audioCtx.sampleRate;
+          // Hack our own linspace(), the built-in version is woefully broken
+          const xResampled = tf
+            .range(0, [Math.ceil(y.size * relativeFs)])
+            .mul(1 / relativeFs);
+          // Assuming the original sampling rate is 1 means that the
+          // relevant bounding indexes are to the left/right of the
+          // truncated form of the new x sample locations.
+          const yLeftIndexes = xResampled
+            .asType("int32")
+            .clipByValue(0, y.shape[0] - 1)
+            .asType("int32");
+          const yRightIndexes = tf.add(yLeftIndexes, tf.scalar(1, "int32"));
+          // The mixture (alpha) value is also 1 - the decimal part
+          const rightSidedAlphas = tf.sub(
+            1,
+            tf.sub(xResampled, xResampled.asType("int32"))
+          );
+          // Do the linear interpolation at each point.
+          const yResampled = tf.add(
+            tf.mul(y.gather(yLeftIndexes), tf.sub(1, rightSidedAlphas)),
+            tf.mul(y.gather(yRightIndexes), rightSidedAlphas)
+          );
+
+          // Do the actual spectrogram now...
+          return tf
+            .sqrt(
+              tf.abs(
+                tf.signal.stft(yResampled, this.frameLength, this.frameStep)
+              )
+            )
+            .expandDims();
+        }
+
+        static get className() {
+          return "Spectrogram";
+        }
+      }
+      tf.serialization.registerClass(Spectrogram);
+
+      // Spectrogram must be registered before calling loadModel().
+      loadModel().then(function(model) {
+        var audioBuffer = new Array(audioCtx.sampleRate * 20).fill(0);
+
+        scriptNode = audioCtx.createScriptProcessor(16384, 1, 1);
+        scriptNode.onaudioprocess = function(event) {
+          audioBuffer = audioBuffer
+            .slice(resampled.length)
+            .concat(Array.from(event.inputBuffer.getChannelData(0)));
+
+          functionOutput.innerHTML = model.predict(
+            tf.tensor(audioBuffer, [1, audioBuffer.length])
+          );
+        };
+
+        source.connect(scriptNode);
+        scriptNode.connect(audioCtx.destination);
+      });
     })
     .catch(function(err) {
-      throw err;
+      console.log("Error with getUserMedia: ", err);
     });
 } else {
-  console.log("getUserMedia not supported on your browser");
+  alert("Your browser is not supported.");
 }
