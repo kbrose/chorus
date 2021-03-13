f4daab54678b7b9baf256c071185ab1fc9027282
Kevin Rose <kevin+gh@maypark.com>
2021-03-13 09:33:32 -0600

    rename data to pipelines

--- cli.py
+++ cli.py
@@ -1,6 +1,6 @@
 import click
 
-from chorus import data as c_data
+from chorus import pipelines
 from chorus import train as c_train
 
 
@@ -17,14 +17,14 @@ def data():
 @data.command('xc-meta', help='download xeno-canto meta data')
 @click.option('-v', '--verbose', is_flag=True, help='Show progress bar.')
 def xeno_meta(verbose: bool):
-    c_data.save_all_xeno_canto_meta(verbose)
+    pipelines.save_all_xeno_canto_meta(verbose)
 
 
 @data.command('xc-audio', help='download xeno-canto audio data')
 @click.option('-v', '--verbose', is_flag=True, help='Show progress bar.')
 @click.option('--redownload', is_flag=True, help="Redownload all files.")
 def xeno_audio(verbose: bool, redownload: bool):
-    c_data.save_all_xeno_canto_audio(verbose, skip_existing=not redownload)
+    pipelines.save_all_xeno_canto_audio(verbose, skip_existing=not redownload)
 
 
 @data.command('xc-to-npy', help='convert audio to .npy data at [SAMPLERATE]')
@@ -32,20 +32,20 @@ def xeno_audio(verbose: bool, redownload: bool):
 @click.option('--reprocess', is_flag=True, help='Reprocess all audio.')
 @click.option('-v', '--verbose', is_flag=True, help='Show progress bar.')
 def xeno_to_numpy(samplerate, reprocess: bool, verbose: bool):
-    c_data.convert_to_numpy(samplerate, verbose, not reprocess)
+    pipelines.convert_to_numpy(samplerate, verbose, not reprocess)
 
 
 @data.command('range-meta', help='download range map meta data')
 @click.option('-v', '--verbose', is_flag=True, help='Show progress bar.')
 @click.option('--redownload', is_flag=True, help="Redownload all files.")
 def range_meta(redownload: bool, verbose: bool):
-    c_data.save_range_map_meta()
+    pipelines.save_range_map_meta()
 
 
 @data.command('range-map', help='download range map data')
 @click.option('-v', '--verbose', is_flag=True, help='Show progress bar.')
 def range_maps(verbose: bool):
-    c_data.save_range_maps(verbose)
+    pipelines.save_range_maps(verbose)
 
 
 @cli.command(help='train the model')
